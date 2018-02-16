31348b127305ecf7f20b9fe1a60b070858f6fa4b
Nicolas Byl <nico@nicolas-byl.eu>
2018-02-16 23:03:09 +0100

    import project

---
+++ src/main/java/de/codecentric/nbyl/statemachinedemo/StateMachineDemoApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.nbyl.statemachinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateMachineDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/de/codecentric/nbyl/statemachinedemo/StateMachineDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package de.codecentric.nbyl.statemachinedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StateMachineDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
